a6d20649bcae9e27f2e7cd8adf93b794fde41ae5
DriftingStarry <doubichenzi@126.com>
2026-06-11 00:40:03 +0800

    feat: execTool for execute command without safe checking

---
+++ src/tools.ts
@@ -0,0 +1,23 @@
+import { tool } from "@langchain/core/tools"
+import {z} from 'zod/v4'
+import util from 'node:util';
+import child_process from 'node:child_process';
+const exec = util.promisify(child_process.exec);
+
+export const execTool = tool(
+    async ({cmd}) => {
+        const {stdout, stderr} = await exec(cmd)
+        return {
+            stdout:stdout,
+            stderr:stderr
+        }
+
+    },
+    {
+        name:'execTool',
+        description:'execute cmd',
+        schema: z.object({
+            cmd:z.string().describe('command to execute, returns cmd stdout and stderr')
+        })
+    }
+)
