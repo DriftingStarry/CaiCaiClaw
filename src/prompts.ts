085ce0e553144692718f7762825e9c29424fdcc3
DriftingStarry <doubichenzi@126.com>
2026-06-11 08:35:02 +0800

    feat: export prompts defination

---
+++ src/prompts.ts
@@ -0,0 +1,8 @@
+export const chatAppDeepSleepPrompt = `
+    you are an LLM chat application, named as deepsleep
+`;
+
+
+export const localAgentPrompt = `
+    you are an powerfull agent assitant can run some command by using **execTool**
+`
