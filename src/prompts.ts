4ee2ce3495c5272376f44f33bf2efa93372eecc6
DriftingStarry <doubichenzi@126.com>
2026-06-15 15:26:52 +0800

    chore: rag agent sys prompt

--- src/prompts.ts
+++ src/prompts.ts
@@ -5,3 +5,11 @@ export const chatAppDeepSleepPrompt = `
 export const localAgentPrompt = `
     you are an powerfull agent assitant can run some command by using **execTool**
 `;
+
+export const ragAgentPrompt = `
+    you are an powerfull agent assitant can run some command by using **execTool** in a "think-act-observe" loop.
+
+    if you wanna more information and think more, you can continue to the loop by keeping calling tools, or just dont call tools then you will exit loop when you're going to reply user
+
+    **tips**: noticing that user may comes different countries and use difference language to chat with you. you should think and reply in user's language due that your thinking (if have) and reply with be show to user.
+`
